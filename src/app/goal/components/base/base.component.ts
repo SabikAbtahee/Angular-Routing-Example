11ef4b6ddf979be620a80babb983a944290aaa5a
DESKTOP-M9HD3C4\Sabik Abtahee <sabikchamp@gmail.com>
2020-10-29 10:40:55 +0600

    Dynamic data passing via different component

--- src/app/goal/components/base/base.component.ts
+++ src/app/goal/components/base/base.component.ts
@@ -1,22 +1,29 @@
+import { TimelineComponent } from './../timeline/timeline.component';
+import { ListComponent } from './../list/list.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-base',
-  templateUrl: './base.component.html',
-  styleUrls: ['./base.component.scss']
+    selector: 'app-base',
+    templateUrl: './base.component.html',
+    styleUrls: ['./base.component.scss']
 })
 export class BaseComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  onOutletLoaded(component){
-      setTimeout(()=>{
-        component.node='somevalue';
+    onOutletLoaded(component) {
+        if (component instanceof ListComponent) {
+            component.nodevar = 'list';
 
-      },5000)
-  }
+        }
+        if (component instanceof TimelineComponent) {
+            component.timelinevar = 'timeline';
+
+        }
+
+    }
 
 }
--- src/app/goal/components/timeline/timeline.component.ts
+++ src/app/goal/components/timeline/timeline.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+    selector: 'app-timeline',
+    templateUrl: './timeline.component.html',
+    styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
-public node;
-  constructor() { }
+    public node;
+    timelinevar
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
